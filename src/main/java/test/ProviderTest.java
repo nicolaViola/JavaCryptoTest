0d035533f52eebc1d2a674d73e4e3b7f7fe3fb4b
nicola <nicola@nicolaPcHp>
2019-09-16 19:13:02 +0200

    Provider

---
+++ src/main/java/test/ProviderTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import java.security.Provider;
+import java.security.Security;
+import java.security.Provider.Service;
+import java.util.Set;
+
+public class ProviderTest {
+
+	public static void main(String[] args) {
+
+		Provider[] providers = Security.getProviders("SecureRandom.SHA1PRNG");
+		
+		for(Provider provider : providers) {
+			System.out.println(provider.getName() + " " + provider.getVersion());
+			
+			Set<Service> services = provider.getServices();
+			
+			for(Service service : services) {
+				System.out.println("* " + service.getAlgorithm());
+			}
+		}
+		
+	}
+
+}
